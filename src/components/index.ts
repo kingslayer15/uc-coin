8af73a954240d71c13efbdd0224c0eeb2dfe1ff9
mblee <343198284@qq.com>
2023-04-02 18:35:31 +0800

    1

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { Account } from './Account'
 export { Connect } from './Connect'
 export { NetworkSwitcher } from './NetworkSwitcher'
-export { GetPledgeInfo, UpdateMintCfg, SetMintCfg, SetPledgeReqire, SetPledgeReturn} from './Contracts'
+export * as Contracts from './Contracts'
